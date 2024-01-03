d6b3a727ffbca7d5f7d34e66278390affc728f53
Kennedy <tmalonga737@gmail.com>
2024-01-03 08:47:37 +0200

    created user model Schema

---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
+
+//the model mongoDB will use in the database
+const User = mongoose.model('User', userSchema);
+
+export default User;
