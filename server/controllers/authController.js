5f038a0665709a13479f63b3eddb7b69f70a9376
Kennedy <tmalonga737@gmail.com>
2024-01-07 20:09:28 +0200

    created sign up api route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,20 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 const signup = async (req, res) => {
+    try {
         const { username, email, password } = req.body;
+        //password hashing using bcrypt
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
         //creating user using the userModel Schema
-        const newUser = new User({ username, email, password })
+        const newUser = new User({ username, email, password: hashedPassword })
         await newUser.save()
 
         res.status(201).json('User created successfully!!')
+    } catch (err) {
+        console.log(err.message)
+    }
 };
 
 module.exports = {
