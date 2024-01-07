517d305fd61ff327d104353c1d025389a28983e3
Kennedy <tmalonga737@gmail.com>
2024-01-07 21:25:41 +0200

    fixed a bug

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,6 +2,8 @@ const errorHandler = ((err, req, res, next) => {
     const statusCode = err.statusCode || 500
     const message = err.message || 'Internal Server Error';
 
+    next(err)
+
     res.status(statusCode).json({
         success: false,
         statusCode,
@@ -9,14 +11,4 @@ const errorHandler = ((err, req, res, next) => {
     })
 })
 
-const newError = (statusCode, message) => {
-    const error = new Error();
-    error.statusCode = statusCode;
-    error.message = message;
-    return error;
-}
-
-module.exports = {
-    errorHandler,
-    newError
-};
+module.exports = errorHandler;
